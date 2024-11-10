aa7d1a12e015aab7e8f45e201655835a49a9857c
Pranav Rawat <144065572+pranavrawat1301@users.noreply.github.com>
2024-11-11 01:37:48 +0530

    Delete Audio Summarizer directory

--- Audio Summarizer/save.py
+++
@@ -1,8 +0,0 @@
-from load_transcript import save_documents_to_faiss
-
-
-# Run once to save to FAISS
-
-#file_path = r"D:\Projects(internship)\LLM based Projects\Audio Summarizer\Power_English_Update.mp3"
-#save_documents_to_faiss(file_path)
-
