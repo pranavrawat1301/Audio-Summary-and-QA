f965d7a91a57c0e8b08f1824fc4cef623731622b
Pranav Rawat <144065572+pranavrawat1301@users.noreply.github.com>
2024-11-12 02:15:22 +0530

    Update summary.py

--- summary.py
+++ summary.py
@@ -23,5 +23,21 @@ def response_generator(file_path):
     
     return " ".join(summaries)
 
+#from concurrent.futures import ThreadPoolExecutor, as_completed
+
+'''def response_generator(file_path):
+    searchDocs = audio_file_to_text(file_path)
+    chunk_size = 1024
+    chunks = [searchDocs[i:i+chunk_size] for i in range(0, len(searchDocs), chunk_size)]
+    
+    summaries = []
+    with ThreadPoolExecutor() as executor:
+        futures = [executor.submit(summarizer, chunk, max_length=150, min_length=50, do_sample=False) for chunk in chunks]
+        for future in as_completed(futures):
+            summary = future.result()
+            summaries.append(summary[0]['summary_text'])
+    
+    return " ".join(summaries)'''
+
 
 
