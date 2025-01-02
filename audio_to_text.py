cc4c99ff82a876e86d0e1a83e00422b347f832a3
Pranav Rawat <144065572+pranavrawat1301@users.noreply.github.com>
2025-01-02 22:39:34 +0530

    Update audio_to_text.py

--- audio_to_text.py
+++ audio_to_text.py
@@ -6,7 +6,7 @@ FAISS_PATH = r"D:\f-ai\Pdf vocal query bot\faiss_local"
 INDEX_NAME = "faiss_index"
 
 def audio_file_to_text(file_path):
-    aai.settings.api_key = "b7c7f111370849ed8f2256ab63364594"
+    aai.settings.api_key = "YOUR API KEY"
     transcriber = aai.Transcriber()
     transcript = transcriber.transcribe(file_path)
     
